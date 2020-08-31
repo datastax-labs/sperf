a9f32eece56a0b1c7887b4460a3600bb12a4c7e0
Ryan Svihla <rs@foundev.pro>
2020-08-31 16:08:26 +0200

    converted diag and humanize to unittest from pytest

--- tests/test_diag.py
+++ tests/test_diag.py
@@ -15,114 +15,116 @@
 """'cass diag' parsing and report writing tests"""
 import os
 import types
-import pytest
+import unittest
 from tests import current_dir, steal_output, make_67_diag_args
 from pysper import env, VERSION
 from pysper.core.diag import parse_diag
 from pysper.diag import find_files
 from pysper.commands.core import diag as diag_cmd
 
-def test_find_files_by_diag_dir():
-    """find logs by diag dir"""
-    config = types.SimpleNamespace()
-    test_dir = os.path.join(current_dir(__file__), "testdata", "diag", "findfiles")
-    config.diag_dir = test_dir
-    config.files = ""
-    files = find_files(config, "my.log")
-    assert len(files) == 4
-    assert os.path.join(test_dir, "nodes", "node1", "my.log") in files
-    assert os.path.join(test_dir, "nodes", "node1", "my.log.1") in files
-    assert os.path.join(test_dir, "nodes", "node1", "my.log.2") in files
-    assert os.path.join(test_dir, "nodes", "node1", "debug.log") not in files
-    assert os.path.join(test_dir, "nodes", "node2", "my.log") in files
-    assert os.path.join(test_dir, "nodes", "node2", "debug.log") not in files
 
+class TestDiagModule(unittest.TestCase):
+    """tests the diag module"""
 
-def test_find_files_by_files_param():
-    """find logs by file name and not just looking in a diag for all matches"""
-    config = types.SimpleNamespace()
-    test_dir = os.path.join(current_dir(__file__), "testdata", "diag", "findfiles")
-    config.diag_dir = ""
-    config.files = os.path.join(test_dir, "nodes", "node1", "my.log") + "," + \
-                    os.path.join(test_dir, "nodes", "node2", "my.log")
-    files = find_files(config, "my.log")
-    assert len(files) == 2
-    assert os.path.join(test_dir, "nodes", "node1", "my.log") in files
-    assert os.path.join(test_dir, "nodes", "node1", "my.log.1") not in files
-    assert os.path.join(test_dir, "nodes", "node1", "my.log.2") not in files
-    assert os.path.join(test_dir, "nodes", "node1", "debug.log") not in files
-    assert os.path.join(test_dir, "nodes", "node2", "my.log") in files
-    assert os.path.join(test_dir, "nodes", "node2", "debug.log") not in files
+    def test_find_files_by_diag_dir(self):
+        """find logs by diag dir"""
+        config = types.SimpleNamespace()
+        test_dir = os.path.join(current_dir(__file__), "testdata", "diag", "findfiles")
+        config.diag_dir = test_dir
+        config.files = ""
+        files = find_files(config, "my.log")
+        self.assertEqual(len(files), 4)
+        self.assertTrue(os.path.join(test_dir, "nodes", "node1", "my.log") in files)
+        self.assertTrue(os.path.join(test_dir, "nodes", "node1", "my.log.1") in files)
+        self.assertTrue(os.path.join(test_dir, "nodes", "node1", "my.log.2") in files)
+        self.assertFalse(os.path.join(test_dir, "nodes", "node1", "debug.log") in files)
+        self.assertTrue(os.path.join(test_dir, "nodes", "node2", "my.log") in files)
+        self.assertFalse(os.path.join(test_dir, "nodes", "node2", "debug.log") in files)
 
-def test_parse_diag():
-    """happy path test for parsing a diag tarball"""
-    config = types.SimpleNamespace()
-    test_dir = os.path.join(current_dir(__file__), "testdata", "diag", "DSE_CLUSTER")
-    config.diag_dir = test_dir
-    config.node_info_prefix = "node_info.json"
-    config.system_log_prefix = "system.log"
-    config.output_log_prefix = "output.log"
-    config.cfstats_prefix = "cfstats"
-    config.block_dev_prefix = "blockdev_report"
-    env.DEBUG = True
-    parsed = {}
-    try:
+    def test_find_files_by_files_param(self):
+        """find logs by file name and not just looking in a diag for all matches"""
+        config = types.SimpleNamespace()
+        test_dir = os.path.join(current_dir(__file__), "testdata", "diag", "findfiles")
+        config.diag_dir = ""
+        config.files = os.path.join(test_dir, "nodes", "node1", "my.log") + "," + \
+                        os.path.join(test_dir, "nodes", "node2", "my.log")
+        files = find_files(config, "my.log")
+        self.assertEqual(len(files), 2)
+        self.assertTrue(os.path.join(test_dir, "nodes", "node1", "my.log") in files)
+        self.assertTrue(os.path.join(test_dir, "nodes", "node1", "my.log.1") not in files)
+        self.assertTrue(os.path.join(test_dir, "nodes", "node1", "my.log.2") not in files)
+        self.assertTrue(os.path.join(test_dir, "nodes", "node1", "debug.log") not in files)
+        self.assertTrue(os.path.join(test_dir, "nodes", "node2", "my.log") in files)
+        self.assertTrue(os.path.join(test_dir, "nodes", "node2", "debug.log") not in files)
+
+    def test_parse_diag(self):
+        """happy path test for parsing a diag tarball"""
+        config = types.SimpleNamespace()
+        test_dir = os.path.join(current_dir(__file__), "testdata", "diag", "DSE_CLUSTER")
+        config.diag_dir = test_dir
+        config.node_info_prefix = "node_info.json"
+        config.system_log_prefix = "system.log"
+        config.output_log_prefix = "output.log"
+        config.cfstats_prefix = "cfstats"
+        config.block_dev_prefix = "blockdev_report"
+        env.DEBUG = True
+        parsed = {}
+        try:
+            parsed = parse_diag(config)
+        finally:
+            env.DEBUG = False
+        self.assertFalse(parsed.get("warnings"))
+        first = parsed.get("configs")[0]
+        self.assertTrue(first['busiest_table_writes'])
+        self.assertEqual(first['busiest_table_writes'][0], 'my_solr.my_table')
+        self.assertEqual("%.2f" % first['busiest_table_writes'][1], '96.18')
+        self.assertEqual(first['busiest_table_reads'][0], 'my_solr.my_table')
+        self.assertEqual("%.2f" % first['busiest_table_reads'][1], '99.76')
+        self.assertEqual(first['threads_per_core'], 1)
+
+    def test_parse_diag_reports_no_files_found(self):
+        """should see missing files in the warning list"""
+        config = types.SimpleNamespace()
+        test_dir = os.path.join(current_dir(__file__), "testdata", "diag", "empty")
+        config.diag_dir = test_dir
+        config.node_info_prefix = "node_info.json"
+        config.system_log_prefix = "system.log"
+        config.output_log_prefix = "output.log"
+        config.cfstats_prefix = "cfstats"
+        config.block_dev_prefix = "blockdev_report"
         parsed = parse_diag(config)
-    finally:
-        env.DEBUG = False
-    assert not parsed.get("warnings")
-    first = parsed.get("configs")[0]
-    assert first['busiest_table_writes']
-    assert first['busiest_table_writes'][0] == 'my_solr.my_table'
-    assert "%.2f" % first['busiest_table_writes'][1] == '96.18'
-    assert first['busiest_table_reads'][0] == 'my_solr.my_table'
-    assert "%.2f" % first['busiest_table_reads'][1] == '99.76'
-    assert first['threads_per_core'] == 1
+        warnings = parsed.get("warnings")
+        self.assertEqual(len(warnings), 4)
+        self.assertTrue("missing output logs: all nodes" in warnings)
+        self.assertTrue("unable to read 'node_info.json'" in warnings)
+        self.assertTrue("missing system logs: all nodes" in warnings)
+        self.assertTrue("missing cfstats: all nodes" in warnings)
 
-def test_parse_diag_reports_no_files_found():
-    """should see missing files in the warning list"""
-    config = types.SimpleNamespace()
-    test_dir = os.path.join(current_dir(__file__), "testdata", "diag", "empty")
-    config.diag_dir = test_dir
-    config.node_info_prefix = "node_info.json"
-    config.system_log_prefix = "system.log"
-    config.output_log_prefix = "output.log"
-    config.cfstats_prefix = "cfstats"
-    config.block_dev_prefix = "blockdev_report"
-    parsed = parse_diag(config)
-    warnings = parsed.get("warnings")
-    assert len(warnings) == 4
-    assert "missing output logs: all nodes" in warnings
-    assert "unable to read 'node_info.json'" in warnings
-    assert "missing system logs: all nodes" in warnings
-    assert "missing cfstats: all nodes" in warnings
+    def test_parse_diag_reports_missing_files(self):
+        """should see missing files in the warning list"""
+        config = types.SimpleNamespace()
+        test_dir = os.path.join(current_dir(__file__), "testdata", "diag", "missing")
+        config.diag_dir = test_dir
+        config.node_info_prefix = "node_info.json"
+        config.system_log_prefix = "system.log"
+        config.output_log_prefix = "output.log"
+        config.cfstats_prefix = "cfstats"
+        config.block_dev_prefix = "blockdev_report"
+        parsed = parse_diag(config)
+        warnings = parsed.get("warnings")
+        self.assertEqual(len(warnings), 4)
+        self.assertTrue("missing output logs: node2" in warnings)
+        self.assertTrue("missing blockdev_reports: node2" in warnings)
+        self.assertTrue("missing system logs: node2" in warnings)
+        self.assertTrue("missing cfstats: node2" in warnings)
 
-def test_parse_diag_reports_missing_files():
-    """should see missing files in the warning list"""
-    config = types.SimpleNamespace()
-    test_dir = os.path.join(current_dir(__file__), "testdata", "diag", "missing")
-    config.diag_dir = test_dir
-    config.node_info_prefix = "node_info.json"
-    config.system_log_prefix = "system.log"
-    config.output_log_prefix = "output.log"
-    config.cfstats_prefix = "cfstats"
-    config.block_dev_prefix = "blockdev_report"
-    parsed = parse_diag(config)
-    warnings = parsed.get("warnings")
-    assert len(warnings) == 4
-    assert "missing output logs: node2" in warnings
-    assert "missing blockdev_reports: node2" in warnings
-    assert "missing system logs: node2" in warnings
-    assert "missing cfstats: node2" in warnings
-
-@pytest.mark.skipif(os.environ.get("TEST_LEVEL") == "fast", reason="fast mode")
-def test_core_diag_integtration():
-    """integration test with 6.7 tarball"""
-    args = make_67_diag_args()
-    def run():
-        diag_cmd.run(args)
-    output = steal_output(run)
-    assert output == "sperf core diag version: %s" % VERSION + """
+    def test_core_diag_integtration(self):
+        """integration test with 6.7 tarball"""
+        args = make_67_diag_args()
+        def run():
+            diag_cmd.run(args)
+        output = steal_output(run)
+        self.assertEqual(output, "sperf core diag version: %s" % VERSION + """
 
 
 configuration #1
@@ -217,4 +219,4 @@ config diff from common:
 
 parser warnings
 ---------------
-no warnings"""
+no warnings""")
