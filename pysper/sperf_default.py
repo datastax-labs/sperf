3ad0d4522685f4b8f64eea11a19eb271fa4fc250
Ryan SVIHLA <tech@foundev.pro>
2021-04-26 07:50:21 +0200

    added support reference link to mmap issues

--- pysper/sperf_default.py
+++ pysper/sperf_default.py
@@ -134,6 +134,7 @@ def _recs_on_configs(recommendations, configs):
                     + "performance on version %s of Cassandra. (SUPPORT-753)"
                     % cassandra_version
                 )
+                issue = issue + " https://support.datastax.com/hc/en-us/articles/360027838911"
                 if issue not in recs_by_issue:
                     recs_by_issue[issue] = {
                         "issue": issue,
@@ -154,6 +155,7 @@ def _recs_on_configs(recommendations, configs):
                     + "with performance on version %s of DSE. (SUPPORT-752)"
                     % dse_version
                 )
+                issue = issue + " https://support.datastax.com/hc/en-us/articles/360028294671"
                 if issue not in recs_by_issue:
                     recs_by_issue[issue] = {
                         "issue": issue,
