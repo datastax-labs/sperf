e49b5464fce131ea71e8cedbed889a99655d23d5
Ryan SVIHLA <ryan.svihla@datastax.com>
2021-07-29 18:29:23 +0200

    fixed changelog script to take dynamic directory

--- scripts/update_change.py
+++ scripts/update_change.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
-with open("CHANGELOG.TXT", "r") as c:
+import os
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+with open(dir_path + "/../CHANGELOG.TXT", "r") as c:
     with open("./pysper/changelog.py", "w") as w:
         w.write('CHANGES = """\n')
         for l in c:
